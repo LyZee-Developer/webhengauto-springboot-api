4d5354adf74083e005694188515be44e5b88ae44
leangsengLy <edeepleang@gmail.com>
2025-12-30 21:53:53 +0700

    add

---
+++ autoPush.sh
@@ -0,0 +1,21 @@
+
+msd="$1"
+if [[ -z $msd ]]; then 
+    echo "please input message to commit our code 👈"
+    exit 1
+fi
+
+branch=$(git branch --show-current)
+if [[ $branch == "master" ]]; then
+    read -p "Are you sure to push code to master 👈 (y/n): " push
+    if [[ $push=="y" || $push=="Y" ]]; then
+            git add .
+            git commit -m "$msd"
+            git push -u origin master
+    else  exit 1
+    fi
+fi 
+
+git add .
+git commit -m "$msd"
+git push -u origin $branch
