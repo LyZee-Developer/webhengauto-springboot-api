7ac095a64a639929775d8e3d609bf6ed2480bead
leangsengLy <edeepleang@gmail.com>
2026-01-12 18:24:05 +0700

    change

--- src/main/java/api/hgseviceweb/config/StaticResourceConfig.java
+++ src/main/java/api/hgseviceweb/config/StaticResourceConfig.java
@@ -7,8 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class StaticResourceConfig implements WebMvcConfigurer{
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-            .addResourceHandler("/upload/**")
-            .addResourceLocations("file:upload/");
+        // registry
+        //     .addResourceHandler("/upload/**")
+        //     .addResourceLocations("file:upload/");
+        registry.addResourceHandler("/upload/**")
+        .addResourceLocations("file:/app/app/upload/");
+            
     }
 }
