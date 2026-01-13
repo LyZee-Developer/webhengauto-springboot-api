72903f6742ebc51c879103776fff65d53199829e
leangsengLy <edeepleang@gmail.com>
2026-01-13 11:37:26 +0700

    change

--- src/main/java/api/hgseviceweb/config/StaticResourceConfig.java
+++ src/main/java/api/hgseviceweb/config/StaticResourceConfig.java
@@ -1,15 +1,10 @@
 package api.hgseviceweb.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class StaticResourceConfig implements WebMvcConfigurer{
-    @Value("${file.upload-img-dir}")
-    private String imageServerLocation;
-    @Value("${file.upload-img-client}")
-    private String imageClientLocation;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // registry
